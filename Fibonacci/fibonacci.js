f471f52a098011b96079bae0d07d08ca285a6bbf
Elvis Garcia <elvisgarcia93@gmail.com>
2022-02-02 22:48:25 -0500

    Add Fibonacci Directory and Solution

---
+++ Fibonacci/fibonacci.js
@@ -0,0 +1,11 @@
+function fibonacci(num){
+    //Base Case 
+    if(num == 1 || num == 2){
+        return 1;
+    }   
+
+    //Recursive Case
+    return fibonacci(num - 2) + fibonacci(num - 1);
+}
+
+console.log(fibonacci(15));
